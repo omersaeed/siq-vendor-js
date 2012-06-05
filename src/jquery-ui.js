af680d2bd280b69bf3cf08aa01b3fca5fec5d121
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-06-05 07:07:02 -0500

    updated to use require 'shim' config

--- src/jquery-ui.js
+++
@@ -1,7 +0,0 @@
-define([
-    './jquery',
-    'order!./jquery-ui/jquery-ui-1.8.18.custom'
-], function($, dummy) {
-    return $;
-});
-
--- src/underscore.js
+++
@@ -1,5 +0,0 @@
-define([
-    'order!./underscore/underscore-1.3.1'
-], function(dummy) {
-    return window._;
-});
